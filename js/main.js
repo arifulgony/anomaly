5e4ece3efc2be8a9d8998eeac0d20817119af1dc
arifulgony <arifulgony@gmail.com>
2023-08-22 23:09:11 +0600

    banner

---
+++ js/main.js
@@ -0,0 +1,7 @@
+ //header
+ function toggleMenu() {
+    var toggle = document.querySelector(".toggle");
+    var nav = document.querySelector(".nav");
+    toggle.classList.toggle("active");
+    nav.classList.toggle("active");
+}
